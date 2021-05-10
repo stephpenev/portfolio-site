282ba4e996c59ace8c88c78927644025bdbdf328
stephp <stephanie.penev@gmail.com>
2021-05-10 13:29:29 -0400

    revising some files and text

--- script/script.js
+++ script/script.js
@@ -1,37 +1,33 @@
-// Toggling menu icon open and closed //
-
-const menuButton = document.querySelector('.menuButton');
-const navMenu = document.querySelector('.navMenu');
-const navList = document.querySelector('.navList');
-const toTop = document.querySelector(".toTop");
-const toContact = document.querySelector(".aboutContact");
-let menuOpen = false;
-
+const myApp = {};
 console.log(`~~ Designed + Developed by Stephanie Penev ~~`);
 
-menuButton.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuButton.classList.add('open');
-    navMenu.classList.add('open');
-    navList.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuButton.classList.remove('open');
-    navMenu.classList.remove('open');
-    navList.classList.remove('open');
-    menuOpen = false;
-  }
-});
+// Toggling menu open and closed //
 
-// Smooth Scroll to sections in Menu
+myApp.toggleMenu = function () {
+  const menuButton = document.querySelector(".menuButton");
+  const navMenu = document.querySelector(".navMenu");
+  const navList = document.querySelector(".navList");
+  const menu = document.querySelector("#menu");
+  let menuOpen = false;
 
-// navList.addEventListener('click', (event) => {
-//   if (event.target.tagName == "LI") {
-//     const type = event.target.className;
-//     const scrollTo = document.querySelector("#" + type);
-//     scrollTo.scrollIntoView({ behavior: "smooth" });
-//   }
-// });
+  menuButton.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuButton.classList.add("open");
+      navMenu.classList.add("open");
+      navList.classList.add("open");
+      menu.setAttribute('aria-expanded', 'true');
+      menuOpen = true;
+    } else {
+      menuButton.classList.remove("open");
+      navMenu.classList.remove("open");
+      navList.classList.remove("open");
+      menu.setAttribute("aria-expanded", "false");
+      menuOpen = false;
+    }
+  });
+};
+
+// Smooth Scroll to sections from Menu
 
 function anchorLinkHandler(e) {
   const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
@@ -54,70 +50,75 @@ function anchorLinkHandler(e) {
       clearInterval(checkIfDone);
     }
   }, 100);
-}
 
-const linksToAnchors = document.querySelectorAll('a[href^="#"]');
-
-linksToAnchors.forEach((each) => (each.onclick = anchorLinkHandler));
+  const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+  linksToAnchors.forEach((each) => (each.onclick = anchorLinkHandler));
+};
 
 // Smooth scroll for To Top and Contact links
 
-// Move empty object to top and namespace other functions:
-myApp = {};
+myApp.scrollButtons = function () {
+  const toContact = document.querySelector(".aboutContact");
+  const scrollToContact = document.getElementById("contact");
+  const rootEl = document.documentElement;
+  const toTop = document.querySelector(".toTop");
+  const scrollToTop = function () {
+    rootEl.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-const rootEl = document.documentElement;
+  toTop.addEventListener("click", scrollToTop);
 
-myApp.scrollToTop = function () {
-  rootEl.scrollTo({
-    top: 0,
-    behavior: "smooth",
+  toContact.addEventListener("click", () => {
+    scrollToContact.scrollIntoView({ behavior: "smooth" });
   });
 };
 
-toTop.addEventListener("click", myApp.scrollToTop);
-
-
-toContact.addEventListener('click', () => {
-  const scrollToContact = document.getElementById("contact");
-  scrollToContact.scrollIntoView({ behavior: "smooth" });
-})
-
 // Toggling dev and design sections visible and invisible //
 
-// Button variables
-const devButton = document.querySelector('.devButton');
-const designButton = document.querySelector('.designButton');
+myApp.toggleProjects = function () {
+  const devButton = document.querySelector(".devButton");
+  const designButton = document.querySelector(".designButton");
+  const devProjects = document.querySelector(".devProjects");
+  const designProjects = document.querySelector(".designProjects");
+  let devOpen = false;
+  let designOpen = false;
 
-// Section variables
-const devProjects = document.querySelector('.devProjects');
-const designProjects = document.querySelector('.designProjects');
-let devOpen = false;
-let designOpen = false;
+  devButton.addEventListener("click", () => {
+    if (!devOpen) {
+      devProjects.classList.add("open");
+      devProjects.classList.remove("close");
+      designProjects.classList.add("close");
+      designProjects.classList.remove("open");
+      devButton.classList.add("underline");
+      designButton.classList.remove("underline");
+    } else {
+      devOpen = false;
+    }
+  });
 
-// Listening for button clicks, adding/removing classes:
+  designButton.addEventListener("click", () => {
+    if (!designOpen) {
+      designProjects.classList.add("open");
+      designProjects.classList.remove("close");
+      devProjects.classList.add("close");
+      devProjects.classList.remove("open");
+      designButton.classList.add("underline");
+      devButton.classList.remove("underline");
+    } else {
+      designOpen = false;
+    }
+  });
+};
 
-devButton.addEventListener('click', ()=> {
-  if (!devOpen) {
-    devProjects.classList.add('open');
-    devProjects.classList.remove('close');
-    designProjects.classList.add('close');
-    designProjects.classList.remove('open');
-    devButton.classList.add('underline');
-    designButton.classList.remove('underline');
-  } else {
-    devOpen = false;
-  }
-});
+// Init App
 
-designButton.addEventListener('click', () => {
-  if (!designOpen) {
-    designProjects.classList.add('open');
-    designProjects.classList.remove('close');
-    devProjects.classList.add('close');
-    devProjects.classList.remove('open');
-    designButton.classList.add('underline');
-    devButton.classList.remove('underline');
-  } else {
-    designOpen = false;
-  }
-});
+myApp.init = function () {
+  myApp.scrollButtons();
+  myApp.toggleMenu();
+  myApp.toggleProjects();
+};
+
+myApp.init();
